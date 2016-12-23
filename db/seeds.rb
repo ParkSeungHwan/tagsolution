2d408da481dd6b84dab4d91465b0fb02f667d549
seunghwan <rr5ys5s@gmail.com>
2016-12-23 14:56:55 +0900

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Admin.create(email: 'admin@tagsolution.kr', password: '910307', password_confirmation: '910307')
+
